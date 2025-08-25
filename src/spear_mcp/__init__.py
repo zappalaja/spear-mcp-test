786ceb684a4c4add3424389cf9e2107f4db39111
Jake Zappala <jake.zappala@noaa.gov>
2025-08-25 09:51:02 -0400

    Add files via upload

---
+++ src/spear_mcp/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.0.1'
+__author__ = 'SPEAR'
+__description__ = 'Test Model Context Protocol server for SPEAR NetCDF Public data.'
+
+from .server import main
+
+__all__ = ['main']
---
+++ src/spear_mcp/_main_.py
@@ -0,0 +1,6 @@
+"""Entry point for running spear_mcp as a module"""
+
+from .server import main
+
+if __name__ == '__main__':
+    main()
